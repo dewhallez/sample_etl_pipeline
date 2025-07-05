1efbd192199c5570b916916c11e98f0cd4bfa85c
Akinwale Akinseye <dewhalles@gmail.com>
2025-07-05 14:28:53 -0400

    fixing github actions error

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -18,8 +18,8 @@ logging.basicConfig(
 def get_engine():
     """Create a SQLAlchemy engine using environment variables."""
     try:
-        DB_USER = os.getenv("DB_USER", "airflow")
-        DB_PASS = os.getenv("DB_PASS", "airflow")
+        DB_USER = os.getenv("DB_USER", "user")
+        DB_PASS = os.getenv("DB_PASS", "password")
         DB_NAME = os.getenv("DB_NAME", "carsales")
         DB_HOST = os.getenv("DB_HOST", "localhost")
         DB_PORT = os.getenv("DB_PORT", "5432")
